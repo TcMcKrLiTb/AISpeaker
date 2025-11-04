53745bfcf0d168cb90e6bfcaf28b9c9a4ad78829
zhangshixiao <tcmckrlitb@gmail.com>
2025-11-04 12:12:12 +0800

    重新整合3

--- TouchGFX/generated/fonts/src/Font_HYJiangNanKai_TTF_15_4bpp_31.cpp
+++
@@ -1,7 +0,0 @@
-#include <touchgfx/hal/Types.hpp>
-
-FONT_GLYPH_LOCATION_FLASH_PRAGMA
-KEEP extern const uint8_t unicodes_HYJiangNanKai_TTF_15_4bpp_31[] FONT_GLYPH_LOCATION_FLASH_ATTRIBUTE = {
-    // Unicode: [0xFF1A, uniFF1A]
-    0x53, 0xBA, 0x01, 0x00, 0x00, 0x6C, 0x3A
-};
